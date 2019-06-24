b67b124457799dce0d655b6b1020939492f4adcd
Aleksandro <wolf.aleks007@gmail.com>
2019-06-24 10:45:12 +0200

    Expo_working

--- App.js
+++ App.js
@@ -3,26 +3,13 @@ import { createDrawerNavigator, createStackNavigator, createAppContainer,DrawerI
 import {StyleSheet,SafeAreaView,ScrollView,Image} from 'react-native';
 import Home from './ScreenHome.js';
 import App2 from './App2.js';
-//import App3 from './App3.js';
-const CustomDrawerComponent=(props)=>(
-  <SafeAreaView style={{flex:1}}>
-      <View style={{height:150,backgroundcolor:'white'}}>
-      <Image source={require('C:\Users\Docebo_Guest1\Desktop\Projects\Prova2\images\Android.png')} style={{height:120,width:120,borderRadius:60}}/>
-      </View>
-      <ScrollView>
-        <DrawerItems/>
-      </ScrollView>
- </SafeAreaView>
-)
+
 const MainNavigator = createStackNavigator({
     Home: {screen: Home},
     App2: {screen: App2},
-    //App3: {screen: App3},
-  }, 
-  {contentComponet:CustomDrawerComponent}
-  );
+  }) 
 
-  const MyDrawerNavigator = createDrawerNavigator({
+const MyDrawerNavigator = createDrawerNavigator({
     Impostazioni: MainNavigator,
     Contattaci: MainNavigator
   })
---
+++ index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { AppRegistry, Dimensions } from 'react-native';
+
+
+import App from './App'
+
+AppRegistry.registerComponent('AppMenu', () => App);
