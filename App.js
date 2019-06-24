3b5b6aa3b404c6ab9799ecdf5827f1c6682582d4
Aleksandro <wolf.aleks007@gmail.com>
2019-06-24 11:44:28 +0200

    Removed_unnecessary

--- App.js
+++ App.js
@@ -2,11 +2,9 @@ import React,{Component} from 'react';
 import { createDrawerNavigator, createStackNavigator, createAppContainer,DrawerItems } from "react-navigation";
 import {StyleSheet,SafeAreaView,ScrollView,Image} from 'react-native';
 import Home from './ScreenHome.js';
-import App2 from './App2.js';
 
 const MainNavigator = createStackNavigator({
     Home: {screen: Home},
-    App2: {screen: App2},
   }) 
 
 const MyDrawerNavigator = createDrawerNavigator({
--- ScreenHome.js
+++ ScreenHome.js
@@ -7,19 +7,15 @@ static navigationOptions= {
 };
 
 
-onPressLearnMore = () => {
 
-    this.props.navigation.navigate('App2');  
-}; 
 
 renderItem = (data) => {
     const {item}  = data;
-    //console.warn(JSON.stringify(item))
     return (  
 
         <View style={{flex:1}}>
 
-        <TouchableHighlight style={styles.buttonStyle2} onPress={this.onPressLearnMore}>
+        <TouchableHighlight style={styles.buttonStyle2}>
           <Text style={styles.textStyle}>{item.title}</Text>
         </TouchableHighlight>
 
@@ -28,13 +24,11 @@ renderItem = (data) => {
 }
 
 render(){
-    const data = [{title: 'Antipasti'},{title: 'Primi'}, {title: 'Secondi'}, {title: 'Dolci'},{title: 'test ciao'}, {title: ' ciao'}]
+    const data = [{title: 'Antipasti'},{title: 'Primi'}, {title: 'Secondi'}, {title: 'Dolci'},{title: 'test ciao'}, {title: ' ciao'}, {title: ' ciao'}]
 
     return (
-        <ImageBackground style = {{width: '100%', height: '100%'}} source = {{uri: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj21_f6kfriAhVFy6QKHaFBBB4QjRx6BAgBEAU&url=http%3A%2F%2Fimmagini.4ever.eu%2Fcibi-e-bevande%2Fhamburger-158973&psig=AOvVaw2Kg_v-aaWU7F8699YoZrpQ&ust=1561191216452454'}}>
-          <FlatList data={data} renderItem={this.renderItem} />
-        </ImageBackground>
-    )
+        <FlatList data={data} renderItem={this.renderItem} />
+      )
     }
 }
 
