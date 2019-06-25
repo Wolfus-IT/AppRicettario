8cc415fd7b05a094db3d100c506f9cd42ef7aea0
Ezzzzz030 <51741771+Ezzzzz030@users.noreply.github.com>
2019-06-25 15:44:40 +0200

    avatar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { createDrawerNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
-
+import { createDrawerNavigator, createAppContainer, createStackNavigator,DrawerItems } from 'react-navigation';
+import { Text, View, StyleSheet,ScrollView,SafeAreaView, Image } from 'react-native';
 import HomeScreen from './pages/home/HomeScreen';
 import Settings from './pages/home/Settings';
 import Catalogo from './pages/ricette/Catalogo';
@@ -10,7 +10,17 @@ import listComponents from './pages/ricette/list/listComponents';
 import recipeSteps from './pages/ricette/steps/recipeSteps';
 import home from './pages/ricette/home/home';
 
-
+const CustomDrawerContentComponent = props => (
+  <ScrollView>
+    <SafeAreaView style={{flex: 1}} forceInset={{ top: 'always', horizontal: 'never' }}>
+    <View style={{alignItems:'center',justifyContent: 'center',marginTop:50}}>
+        <Image source={require('./image/home1.jpg')} style={{width: 100, height: 100,borderRadius:60}}/>
+      
+    </View>
+      <DrawerItems {...props} />
+    </SafeAreaView>
+  </ScrollView>
+);
 const MyStackNavigator = createStackNavigator({
   Home: {
     screen: HomeScreen,
@@ -35,6 +45,9 @@ const MyDrawerNavigator = createDrawerNavigator({
   Home: MyStackNavigator,
   ConcatUs: { screen: ConcatUs },
   Settings: { screen: Settings }
+},
+{
+  contentComponent: CustomDrawerContentComponent
 })
 
 export default createAppContainer(MyDrawerNavigator);
